d6daab8b631f5827586fc0e68dc5d7c496fb96ee
sahDipti <123860643+sahDipti@users.noreply.github.com>
2023-07-13 23:17:07 +0545

    Add files via upload

---
+++ lib/model/place.dart
@@ -0,0 +1,32 @@
+import 'package:uuid/uuid.dart';
+import 'dart:io';
+
+const uuid = Uuid();
+
+class PlaceLocation{
+  const PlaceLocation({
+    required this.address,
+    required this.latitude,
+    required this.longitude,
+  });
+
+  final double latitude;
+  final double longitude;
+  final String address;
+
+}
+
+class Place {
+  Place({
+    required this.title,
+    required this.image,
+    required this.location,
+    String? id,
+    }) : id= id ?? uuid.v4();
+
+  final String title;
+  final String id;
+  final File image;
+  final PlaceLocation location;
+
+}
---
+++ lib/screens/places_detail.dart
@@ -0,0 +1,82 @@
+import 'package:favorite_places/model/place.dart';
+import 'package:favorite_places/screens/map.dart';
+import 'package:flutter/material.dart';
+
+
+class PlaceDetailsScreen extends StatelessWidget{
+  const PlaceDetailsScreen({super.key,required this.place});
+
+  final Place place;
+
+  String get LocationImage { 
+    final lat= place.location.latitude;
+    final lng= place.location.longitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyDLcwxUggPZo9lcbH0TB4Crq5SJjtj4ag';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(place.title),
+      ),
+      body: Stack(
+        children: [
+          Image.file(
+            place.image,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: (){
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (ctx) => MapScreen(
+                          location: place.location,
+                          isSelecting: false,
+                        ),
+                        )
+                    );
+                  },
+                  child: CircleAvatar(
+                    radius: 70,
+                    backgroundImage: NetworkImage(LocationImage),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 16,
+                  ),
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.transparent,
+                        Colors.black54,
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Text(place.location.address,
+                  textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+                  ),
+                ),
+              ],
+          ))
+        ],
+      ),
+    );
+  }
+}
